7795618a91787ea86c98720827eb337ecb0fc575
adit-fau <adit@fau.edu>
2014-04-13 20:34:36 -0400

    Update GameTimer.java

--- GameTimer.java
+++ GameTimer.java
@@ -1,1 +1,32 @@
-G
+package mineSweeper;
+
+public class GameTimer {
+	
+	public void start()
+	{
+		startTime=System.currentTimeMillis();
+	}
+	
+	public long giveTime()
+	{
+		return (System.currentTimeMillis()-startTime);
+	}
+	
+	public void stopTime()
+	{
+		stopTime=System.currentTimeMillis()-startTime;
+	}
+	
+	public long giveStopTime()
+	{
+		return stopTime;
+	}
+	public void zeroTime()
+	{
+		stopTime=0;
+	}
+	
+	private long stopTime;
+	private long startTime;
+
+}
